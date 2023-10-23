c62e990b3207327d6e5004858bf954f6426b4075
Julio Arias <julioariasvelez10@gmail.com>
2023-10-23 10:46:50 -0500

    build.sh test2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=julio  --email=julio@example.com
+DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=evelyn777  --email=julio@example.com
 DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=evelyn777 --email=julio@example.com
 
 python manage.py collectstatic --no-input
