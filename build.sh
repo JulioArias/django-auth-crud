70b841989f81382270db9d18f1659ab04160b1de
Julio Arias <julioariasvelez10@gmail.com>
2023-10-23 10:44:16 -0500

    build.sh test2

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=julio  --email=julio@example.com
+DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --no-input --username=evelyn777 --email=julio@example.com
 
 python manage.py collectstatic --no-input
 python manage.py migrate
